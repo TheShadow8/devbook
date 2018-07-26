866addb7e40b5f8a53cf8e88f8338c3e3e12e70c
TheShadow8 <35181893+TheShadow8@users.noreply.github.com>
2018-07-26 16:05:20 -0400

    up profile1

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
 
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -20,8 +22,8 @@ class Navbar extends Component {
         <li className="nav-item">
           <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link" >
             <img className="rounded-circle" src={user.avatar} alt={user.name} style={{ width: '25px', marginRight: '5px' }} />
-          {' '}
-          Logout
+            {' '}
+            Logout
           </a>
         </li>
 
@@ -87,4 +89,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
