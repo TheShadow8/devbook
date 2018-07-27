a3fc9b26b10b1c218eb14bfc64b23fdfcc4ebdd1
TheShadow8 <35181893+TheShadow8@users.noreply.github.com>
2018-07-27 14:05:56 -0400

    redeploy

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
-
 const app = express();
 
 // Body parser middleware
@@ -19,7 +18,8 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose
+    .connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
@@ -29,14 +29,16 @@ app.use(passport.initialize());
 // Passport Config
 require('./config/passport')(passport);
 
-// Use Routes 
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Awecwe static assets if in production 
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
