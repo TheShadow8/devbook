e4173be92c7342f6a8fcaa6dca1052df88bf0dd7
TheShadow8 <35181893+TheShadow8@users.noreply.github.com>
2018-07-27 14:27:25 -0400

    deploy fixed

--- server.js
+++ server.js
@@ -44,5 +44,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000);
+app.listen(port, () => console.log(`Server running on port ${port}`));
